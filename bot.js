e96f7315bbc4e4d17366335aa99b27818b7e57d1
lolsuperscratch <lolsuperscratch@hotmail.com>
2018-08-08 21:12:56 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,22 @@ client.on('guildMemberAdd', function (member) {
     }
 });
 
+client.on("guildCreate", guild => {
+  var found = false;
+  guild.channels.forEach(function(channel, id) {
+      // If a channel is already found, nothing more needs to be done
+      if(found == true || channel.type != "text") {
+        return;
+      }
+      // If the channel isn't found and the bot has permission to 
+      // send and read messages in the channel, send a welcome message there
+      if(guild.me.permissionsIn(channel).has("SEND_MESSAGES") && guild.me.permissionsIn(channel).has("VIEW_CHANNEL")) {
+        found = true;
+        return channel.send("i welcome to users, Yayayaya")
+      }
+  })
+});
+
 
 // bot end
 client.login(process.env.BOT_TOKEN);
