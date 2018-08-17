46cc91a4de0e94c4a0120fc703b5591bb36b1b23
lolsuperscratch <lolsuperscratch@hotmail.com>
2018-08-17 21:54:27 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,50 +1,11 @@
 
 
 // setup
-const Discord = require('discord.js');
+const MPP = require('http://www.multiplayerpiano.com/Client.js');
+const client = new Client('ws://www.multiplayerpiano.com:443')
 
 
-const client = new Discord.Client();
 
-
+// waiting for update
 // bot start
 
-client.on('ready', function () {
-  console.log('bot ready');
-  
-});
-
-
-
-
-client.on('message', function (message) {
-  if (!message.author.bot) {
-     if (message.content == "aroundworld game") {
-        message.channel.send("check your dms")
-        message.author.send("https://aroundworld.bubbleapps.io")
-     }
-     if (message.content == "aroundworld help") {
-        message.channel.send("check your dms")
-        message.author.send("`dice` `ping` `game`")
-     }
-     if (message.content == "aroundworld ping") {
-        message.channel.send("Pong! `" + client.ping + "`")
-        
-     }
-     if (message.content == "aroundworld dice") {
-        message.channel.send("user did roll a dice! `" + Math.floor(Math.random() * 8) + "`")
-        
-     }
-     
-  }
-  if(message.content.indexOf("wtf") > -1) {
-    message.react("😂")
-  }
-  if(message.content.indexOf("gay") > -1) {
-    message.react("😂")
-  }
-});
-
-
-// bot end
-client.login(process.env.BOT_TOKEN);
